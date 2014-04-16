0fbcd4ba34f8a5ae670fbda5deb1b73a41640507
Patrick Kimber <code@pkimber.net>
2014-04-16 16:27:37 +0100

    flakes

--- example/tests/scenario.py
+++ example/tests/scenario.py
@@ -13,7 +13,7 @@ from example.tests.model_maker import make_sales_ledger
 
 def default_scenario_pay():
     pencil = make_product('Pencil', 'pencil', Decimal('1.32'))
-    payment = make_payment(
+    make_payment(
         'test@pkimber.net',
         pencil,
         'Colour pencils',
@@ -21,8 +21,7 @@ def default_scenario_pay():
         Decimal('10.00'),
         make_sales_ledger('Carol'),
     )
-    #print('make_payment: {}'.format(payment.description))
-    payment = make_payment(
+    make_payment(
         'a@pkimber.net',
         pencil,
         'Pencils',
@@ -30,4 +29,3 @@ def default_scenario_pay():
         Decimal('2.50'),
         make_sales_ledger('Andi'),
     )
-    #print('make_payment: {}'.format(payment.description))
