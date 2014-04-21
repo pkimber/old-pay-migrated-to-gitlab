bf652eb742db744b4f1948f0bc159bc8e8cdbfdf
Patrick Kimber <code@pkimber.net>
2014-04-21 09:18:48 +0100

    migration

---
+++ pay/migrations/0001_initial.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Product'
+        db.create_table('pay_product', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(blank=True, auto_now_add=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(blank=True, auto_now=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=8, decimal_places=2)),
+        ))
+        db.send_create_signal('pay', ['Product'])
+
+        # Adding model 'PaymentState'
+        db.create_table('pay_paymentstate', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(blank=True, auto_now_add=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(blank=True, auto_now=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
+        ))
+        db.send_create_signal('pay', ['PaymentState'])
+
+        # Adding model 'Payment'
+        db.create_table('pay_payment', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(blank=True, auto_now_add=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(blank=True, auto_now=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('product', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['pay.Product'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('quantity', self.gf('django.db.models.fields.IntegerField')()),
+            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=8, decimal_places=2)),
+            ('state', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['pay.PaymentState'])),
+            ('url', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
+        ))
+        db.send_create_signal('pay', ['Payment'])
+
+        # Adding unique constraint on 'Payment', fields ['object_id', 'content_type']
+        db.create_unique('pay_payment', ['object_id', 'content_type_id'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'Payment', fields ['object_id', 'content_type']
+        db.delete_unique('pay_payment', ['object_id', 'content_type_id'])
+
+        # Deleting model 'Product'
+        db.delete_table('pay_product')
+
+        # Deleting model 'PaymentState'
+        db.delete_table('pay_paymentstate')
+
+        # Deleting model 'Payment'
+        db.delete_table('pay_payment')
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'ordering': "('name',)", 'db_table': "'django_content_type'", 'object_name': 'ContentType'},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'pay.payment': {
+            'Meta': {'unique_together': "(('object_id', 'content_type'),)", 'ordering': "('pk',)", 'object_name': 'Payment'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now_add': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pay.Product']"}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pay.PaymentState']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'pay.paymentstate': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'PaymentState'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now_add': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        'pay.product': {
+            'Meta': {'ordering': "('slug',)", 'object_name': 'Product'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now_add': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['pay']
