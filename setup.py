2f382b6a46d00f4025074dfad7d246fd1f1448cb
Patrick Kimber <code@pkimber.net>
2014-06-08 22:09:54 +0100

    version 0.1.08

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/pay/*.*',
         ],
     },
-    version='0.1.07',
+    version='0.1.08',
     description='pay',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
