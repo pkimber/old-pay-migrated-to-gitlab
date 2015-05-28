f78f95c3bf7d9bab9332f15fbc6325843d0da9a0
Patrick Kimber <code@pkimber.net>
2015-05-28 22:01:25 +0100

    transfer payment data to payment list #906

--- pay/migrations/0004_auto_20150528_1908.py
+++ pay/migrations/0004_auto_20150528_1908.py
@@ -2,15 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import finance.models
 from decimal import Decimal
+import finance.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('finance', '0002_auto_20150526_1127'),
         ('stock', '0001_initial'),
+        ('finance', '0003_auto_20150526_1908'),
         ('pay', '0003_auto_20141115_1119'),
     ]
 
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PaymentLine',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('modified', models.DateTimeField(auto_now=True)),
                 ('line_number', models.IntegerField()),
@@ -26,26 +26,14 @@ class Migration(migrations.Migration):
                 ('units', models.CharField(max_length=5)),
                 ('net', models.DecimalField(max_digits=8, decimal_places=2)),
                 ('vat', models.DecimalField(max_digits=8, decimal_places=2)),
-                ('save_price', models.DecimalField(default=Decimal('0'), max_digits=8, help_text='Price of the product when the line was saved.', decimal_places=2)),
-                ('save_vat_rate', models.DecimalField(default=Decimal('0'), max_digits=5, help_text='VAT rate when the line was saved.', decimal_places=3)),
+                ('save_price', models.DecimalField(max_digits=8, help_text='Price of the product when the line was saved.', default=Decimal('0'), decimal_places=2)),
+                ('save_vat_rate', models.DecimalField(max_digits=5, help_text='VAT rate when the line was saved.', default=Decimal('0'), decimal_places=3)),
             ],
             options={
-                'verbose_name': 'Payment line',
                 'ordering': ['line_number'],
+                'verbose_name': 'Payment line',
                 'verbose_name_plural': 'Payment lines',
             },
-        ),
-        migrations.RemoveField(
-            model_name='payment',
-            name='price',
-        ),
-        migrations.RemoveField(
-            model_name='payment',
-            name='quantity',
-        ),
-        migrations.RemoveField(
-            model_name='payment',
-            name='title',
         ),
         migrations.AlterField(
             model_name='payment',
@@ -70,7 +58,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='paymentline',
             name='vat_code',
-            field=models.ForeignKey(default=finance.models.legacy_vat_code, to='finance.VatCode'),
+            field=models.ForeignKey(to='finance.VatCode', default=finance.models.legacy_vat_code),
         ),
         migrations.AlterUniqueTogether(
             name='paymentline',
