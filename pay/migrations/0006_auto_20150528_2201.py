721a593eb57d41685e43ce9a978f58cbe9c36380
Patrick Kimber <code@pkimber.net>
2015-06-27 09:42:14 +0100

    revert - remove latest migration to help us with switch to 'checkout' #644

---
+++ pay/migrations/0006_auto_20150528_2201.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pay', '0005_auto_20150528_1909'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='payment',
+            name='price',
+        ),
+        migrations.RemoveField(
+            model_name='payment',
+            name='quantity',
+        ),
+        migrations.RemoveField(
+            model_name='payment',
+            name='title',
+        ),
+    ]
