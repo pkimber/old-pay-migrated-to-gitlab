cd619feab2db4406d2127c4d3c785c29ff222586
Patrick Kimber <code@pkimber.net>
2015-09-29 18:28:24 +0100

    version 0.1.31

--- setup.py
+++ setup.py
@@ -23,12 +23,11 @@ setup(
     packages=['pay', 'pay.migrations', 'pay.tests', 'pay.management', 'pay.management.commands'],
     package_data={
         'pay': [
-            'static/*.*',
             'templates/*.*',
             'templates/pay/*.*',
         ],
     },
-    version='0.1.30',
+    version='0.1.31',
     description='pay',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
@@ -42,6 +41,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Framework :: Django :: 1.8',
         'Topic :: Office/Business :: Scheduling',
     ],
     long_description=get_readme(),
